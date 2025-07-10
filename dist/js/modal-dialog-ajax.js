647d4c5ff39443e12ae383fd5a7c5faaec786c93
JaceRider <jacerider@gmail.com>
2025-07-10 11:26:58 -0500

    :art: Compiled files

--- dist/js/modal-dialog-ajax.js
+++ dist/js/modal-dialog-ajax.js
@@ -1,22 +1,29 @@
 (function(o) {
-  o.AjaxCommands && (o.AjaxCommands.prototype.openDialog = function(n, e, a) {
+  o.AjaxCommands && (o.AjaxCommands.prototype.openDialog = function(a, e, s) {
     if (o.neoModal) {
       let t = {};
-      typeof e.settings == "object" && (t = Object.assign({}, t, e.settings)), e.data && (t.content = e.data), t.onBeforeOpen = (s) => {
-        const i = s.getContent();
-        i && i.querySelectorAll(".dialog-cancel").forEach((c) => {
-          c.addEventListener("click", (l) => {
-            s.close(), l.preventDefault(), l.stopPropagation();
+      typeof e.settings == "object" && (t = Object.assign({}, t, e.settings)), e.data && (t.content = e.data), t.onBeforeOpen = (n) => {
+        const i = n.getContent();
+        i && i.querySelectorAll(".dialog-cancel").forEach((d) => {
+          d.addEventListener("click", (l) => {
+            n.close(), l.preventDefault(), l.stopPropagation();
           });
         });
       }, (typeof t.nest > "u" || t.nest === !1 || t.nest === "false") && o.neoModal.close(), o.neoModal.open(t);
     }
-  }, o.AjaxCommands.prototype.closeDialog = function(n, e, a) {
+  }, o.AjaxCommands.prototype.closeDialog = function(a, e, s) {
     o.neoModal && o.neoModal.close();
-  }, o.AjaxCommands.prototype.setDialogOption = function(n, e, a) {
+  }, o.AjaxCommands.prototype.setDialogOption = function(a, e, s) {
     console.log("Not yet supported in Neo Modal.", "setDialogOption");
-  }, o.AjaxCommands.prototype.openModalDialogWithUrl = function(n, e, a) {
-    console.log("Not yet supported in Neo Modal.", "openModalDialogWithUrl");
+  }, o.AjaxCommands.prototype.openModalDialogWithUrl = function(a, e, s) {
+    const t = e.dialogOptions || {}, n = {
+      progress: { type: "throbber" },
+      dialogType: "modal",
+      dialog: t,
+      url: e.url,
+      httpMethod: "GET"
+    };
+    o.ajax(n).execute();
   });
 })(Drupal);
 //# sourceMappingURL=modal-dialog-ajax.js.map
